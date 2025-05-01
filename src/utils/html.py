0c1a873ac60127be9465989754c834cbc919fb16
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-05-01 11:20:37 -0300

    add feature que remove \n do excerpt

--- src/utils/html.py
+++ src/utils/html.py
@@ -2,4 +2,6 @@ from bs4 import BeautifulSoup
 
 def clean_html(html_content: str) -> str:
     soup = BeautifulSoup(html_content, "html.parser")
-    return soup.get_text()
+    # Get text content and remove explicit newlines
+    text = soup.get_text()
+    return text.replace('\n', '')
